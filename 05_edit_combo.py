bbc2358a5eee28dd38546f2c7e4d0517ce920a9f
WronglyInspired <wonge2@middleton.school.nz>
2023-04-12 20:47:58 +1200

    Update 05_edit_combo.py
    First fully functional version

--- 05_edit_combo.py
+++ 05_edit_combo.py
@@ -15,6 +15,9 @@ def choose_item(combo):
 
     chosen_item_text = buttonbox("Choose item", choices=items)
 
+    if chosen_item_text in ("Save", "Cancel"):
+        return chosen_item_text
+
     # code that converts item from its text form "name $0.00" to code form
     # this will not work if multiple items have same name within combo
     chosen_item_name = chosen_item_text.split("$")[0].strip()
@@ -36,7 +39,6 @@ def get_new_price():
 
 
 def choose_attribute(item):
-
     attributes = item
     attributes[1] = f"${attributes[1]:.2f}"  # format float as a currency str
 
@@ -48,56 +50,46 @@ def choose_attribute(item):
 
 
 def set_attribute(combo, item, attribute):
-
-    # converts string back to float
-    # item[1] = float(attribute.strip("$"))
+    item_temp = item.copy()
 
     # if price selected
     if attribute[0] == "$":
-
         new_price = get_new_price()
-
-        item[1] = new_price
+        item_temp[1] = new_price
 
     # if name selected
     else:
         new_name = enterbox("Enter new name")
+        item_temp[0] = new_name
 
-        item[0] = new_name
-
-    print(65, item)
-    return item
+    return item_temp
 
 
 def edit(menu, selected_combo):
-    # initialise a temporary version of the combo, to either save or delete
-    combo_temp = menu[selected_combo]
+    # create a copy of the selected combo
+    combo_temp = menu[selected_combo].copy()
 
     while True:
         # display possible items to choose, output selected item
         selected_item = choose_item(combo_temp)
         if selected_item == "Save":
-            return combo_temp
+            menu[selected_combo] = combo_temp
+            return menu
         elif selected_item == "Cancel":
-            return menu[selected_combo]
+            print("cancel", menu)
+            return menu
         else:
-
             # display attributes of item to choose, output selected attribute
             selected_attribute = choose_attribute(selected_item)
 
-
             # allow user to re-enter attribute, output new item
             item_temp = set_attribute(combo_temp, selected_item,
-                                  selected_attribute)
-
+                                      selected_attribute)
 
             # set new item in combo temp
-            for item in combo_temp:
-                if item[0] == selected_item:
-                    combo_temp[item] = item_temp
-
-
-
+            for i, item in enumerate(combo_temp):
+                if item[0] == selected_item[0]:
+                    combo_temp[i] = item_temp
 
 
 # Data storage
@@ -119,4 +111,6 @@ burger_combo_menu = {
     ],
 }
 
-edit(burger_combo_menu, "Super")
+chosen_combo = "Value"  # placeholder input
+new_menu = edit(burger_combo_menu, chosen_combo)
+print(new_menu)
